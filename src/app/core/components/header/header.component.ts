4bf32eff3887b558c30d9ae438c27db93deb96c4
Anderson Nascimento <adersonnascimento.parceiro@dnitservices.com>
2022-11-30 15:10:38 -0300

    Projeto que aplica um tema dark a tela

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,28 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  public icon: string = ETheme.ICON_MOON;
+  public textTheme: string = ETheme.TEXT_MOON
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      this.textTheme = ETheme.ICON_SUN;
+      return (this.icon =ETheme.ICON_SUN);
+    }
+    this.textTheme = ETheme.ICON_MOON;
+    return (this.icon =ETheme.ICON_MOON);
+  }
+}
